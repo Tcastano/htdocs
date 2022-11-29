4f0f0031c52b49ccba85c9c76f9ab50f992b39af
Tizziano Nahuel <Nahuel210403@gmail.com>
2022-11-29 10:26:40 +0100

    #

--- 1_arrays.php
+++ 1_arrays.php
@@ -7,3 +7,15 @@
  * Añadir por cada estudiante, una clave más "DWEC" con números aleatorios entre 1 y 10.
  * 
  */
+$notas=[];
+ $alumnos=["hugo","marta","fabio","siro","mario"];
+
+ $curso=["DWEC","DWES"];
+
+ for ($i=0; $i <5 ; $i++) { 
+    $notas
+ }
+
+ foreach ($alumnos as $key => $value) {
+    
+ }
